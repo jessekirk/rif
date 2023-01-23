e95c3c9c8eaa4a41c06b19a12b82b33d8b7d0966
jesse kirk <jesse.kirk.1.ctr@us.af.mil>
2023-01-23 08:10:47 -0600

    initial commit of the "rif" project

---
+++ Rif/Dts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace Rif
+{
+    [Cmdlet(VerbsCommon.Get, "RifDtsUtc")]
+    public class Dts : Cmdlet
+    {
+        protected override void ProcessRecord()
+        {
+            WriteObject(DateTime.UtcNow.ToString("yyyyMMddTHHmmssfffZ"));
+        }
+    }
+}
---
+++ Rif/Ionic.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Text;
+using Ionic.Zip;
+
+namespace Rif
+{
+	internal class Ionic
+	{
+
+	}
+}
